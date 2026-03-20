9257bdbb02bfb578f2ef4a002cdfdf856e0ed7ca
nueng1157 <nueng1157@gmail.com>
2026-03-20 20:53:58 +0700

    operation 3

---
+++ stringoperator.php
@@ -0,0 +1,20 @@
+<?php
+
+$name ="onenueng";
+
+echo $name." studio<br>";
+
+echo "<hr>";
+
+echo $name;
+
+$name.=" studio";
+echo "<hr>";
+
+echo $name;
+echo "<br>";
+echo $name.=" .com";
+
+
+
+?>
