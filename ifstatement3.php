3cca5e1e819346e89b5a65b972b9a3e6f60bc09b
nueng1157 <nueng1157@gmail.com>
2026-03-25 10:45:37 +0700

    if statement2

--- ifstatement3.php
+++ ifstatement3.php
@@ -1,19 +1,19 @@
 <?php
     
     $balance = 1000;
-    $withdraw = 0;
+    $withdraw =1000;
     $a = $withdraw<=$balance; // ผลการเปรียบเทียบจำนวนเงิน
     $b = $withdraw>0; // เงินที่ถอนต้องมีค่ามากกว่า 0 
 
 
     echo "เงินในบัญชี  =" .$balance."<br>";
-    echo "จำนวนเงินที่ถอน  =" .$withdraw."<br>";
+    echo "จำนวนเงินที่ถอน  = " .$withdraw."<br>";
     echo "<hr>";
 
         //if($a && $b){
-        if($a ||$b){
+        if(!($withdraw<=$balance) || $withdraw>0){
         echo "สามารถกดเงินได้ <br>";
-        $balance =$balance-$withdraw; //คำนวณยอดคงเหลือ
+        $balance -=$withdraw; //คำนวณยอดคงเหลือ
         echo "ยอดคงเหลือ =" .$balance. "<br>";
     }else{
         echo "ยอดเงินคงเหลือไม่พอ<br>";
